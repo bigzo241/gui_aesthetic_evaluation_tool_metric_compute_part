f76d08c36a97b769185e6c1142d1e2ce12e5d59a
bigzo <zomamadou9@gmail.com>
2023-02-28 16:36:54 +0000

    Augmentation de la taille de l'entete de la requete HTTP

--- src/main/java/ujkz/aestheticEvaluation/metricCalculator/controller/CalculatorController.java
+++ src/main/java/ujkz/aestheticEvaluation/metricCalculator/controller/CalculatorController.java
@@ -39,51 +39,53 @@ public class CalculatorController {
         System.out.println("Balance : " + BM.floatValue());
 
         Equilibrium equilibrium = Equilibrium.getInstance();
-        Double EM = equilibrium.computeMetric(composantList, 798.0, 381.0);
+        Double EM = equilibrium.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Equilibre : " + EM.floatValue());
 
         Symmetry symmetry = Symmetry.getInstance();
-        Double SYM = symmetry.computeMetric(composantList, 798.0, 381.0);
+        Double SYM = symmetry.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Symmetry : " + SYM.floatValue());
 
         Sequence sequence = Sequence.getInstance();
-        Double SQM = sequence.computeMetric(composantList, 798.0, 381.0);
+        Double SQM = sequence.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Sequence : " + SQM.floatValue());
 
         Cohesion cohesion = Cohesion.getInstance();
-        Double CM = cohesion.computeMetric(composantList, 798.0, 381.0);
+        Double CM = cohesion.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Cohesion : " + CM.floatValue());
 
         Unity unity = Unity.getInstance();
-        Double UM = unity.computeMetric(composantList, 798.0, 381.0);
+        Double UM = unity.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Unity : " + UM.floatValue());
 
         Simplicity simplicity = Simplicity.getInstance();
-        Double SM = simplicity.computeMetric(composantList, 798.0, 381.0);
+        Double SM = simplicity.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Simplicity : " + SM.floatValue());
 
         Density density = Density.getInstance();
-        Double DM = density.computeMetric(composantList, 798.0, 381.0);
+        Double DM = density.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Density : " + DM.floatValue());
 
         Economy economy  = Economy.getInstance();
-        Double ECM = economy.computeMetric(composantList, 798.0, 381.0);
+        Double ECM = economy.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Economy : " + ECM.floatValue());
 
         Homogeneity homogeneity = Homogeneity.getInstance();
-        Double HM = homogeneity.computeMetric(composantList, 798.0, 381.0);
+        Double HM = homogeneity.computeMetric(composantList, width_frame, height_frame);
         System.out.println("Homogeneity : " + HM.floatValue());
 
-        jsonObject.put("Balance", BM);
-        jsonObject.put("Equilibrium", EM);
-        jsonObject.put("Symmetry", SYM);
-        jsonObject.put("Sequence", SQM);
-        jsonObject.put("Cohesion", CM);
-        jsonObject.put("Unity", UM);
-        jsonObject.put("Simplicity", SM);
-        jsonObject.put("Density", DM);
-        jsonObject.put("Economy", ECM);
-        jsonObject.put("Homogeneity", HM);
+
+
+        jsonObject.put("Balance", BM.floatValue());
+        jsonObject.put("Equilibrium", EM.floatValue());
+        jsonObject.put("Symmetry", SYM.floatValue());
+        jsonObject.put("Sequence", SQM.floatValue());
+        jsonObject.put("Cohesion", CM.floatValue());
+        jsonObject.put("Unity", UM.floatValue());
+        jsonObject.put("Simplicity", SM.floatValue());
+        jsonObject.put("Density", DM.floatValue());
+        jsonObject.put("Economy", ECM.floatValue());
+        jsonObject.put("Homogeneity", HM.floatValue());
         return jsonObject;
     }
 }
